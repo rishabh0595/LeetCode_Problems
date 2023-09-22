21f362953066649e3341e1ccf6af41743d55f1a7
Rishab Trivedi <rishabh0595@gmail.com>
2023-09-23 04:38:07 +0530

    Update 643. Maximum Average Subarray I.py

--- 643. Maximum Average Subarray I.py
+++ 643. Maximum Average Subarray I.py
@@ -1,12 +1,23 @@
 def findMaxAverage(arr,k):
-    n = len(arr)
-    window_sum = sum(arr[:k])
-    max_sum = window_sum
-    for i in range(n - k):
-        window_sum = window_sum - arr[i] + arr[i + k]
-        max_sum = max(window_sum, max_sum)
+    sum=0
+    n=len(nums)
+    windowSum=0
+  
+  # Sum of window
+    for i in range (k):
+        windowSum +=nums[i]
+        
+    maxSum = windowSum
 
-    return max_sum/k    
+    # Iterate from k to n
+    for i in range (k,len(nums)):
+        # remove starting element and add last element
+        windowSum = windowSum + nums[i] - nums[i-k]
+        maxSum= max(windowSum,maxSum) # Maximum Sum
+    
+    # Return Average
+    return (maxSum/k)
+  
 
 nums=[9,7,3,5,6,2,0,8,1,9]
 k=5
