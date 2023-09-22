6ea9df5ea5d6fd2060e5e28d375599517cc4a3ac
Rishab Trivedi <rishabh0595@gmail.com>
2023-09-23 04:36:43 +0530

    Update 735. Asteroid Collision.py

--- 735. Asteroid Collision.py
+++ 735. Asteroid Collision.py
@@ -1,17 +1,27 @@
 def asteroidCollision(asteroids):
-    print("*****")
-    ans = []
-    for new in asteroids:
-        while ans and new < 0 < ans[-1]:
-            if ans[-1] < -new:
-                ans.pop()
+    stack = []
+
+    # Iterate over asteroids
+    for asteroid in asteroids:
+        
+        # if stack top is positive and asteroid is negative
+        while stack and stack[-1] > 0 and asteroid < 0:
+
+            # if asteroid is greater then remove the current stack top element
+            if abs(stack[-1]) < abs(asteroid):
+                stack.pop()
                 continue
-            elif ans[-1] == -new:
-                ans.pop()
+            
+            # If equal remove top and dont add current asteroid
+            elif abs(stack[-1]) == abs(asteroid):
+                stack.pop()
             break
+
         else:
-            ans.append(new)
-    return ans
+            stack.append(asteroid)
+
+    # return Stack
+    return stack
 
 asteroids=[5,10,-5]
 print(asteroidCollision(asteroids))
